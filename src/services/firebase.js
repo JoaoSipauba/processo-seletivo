a8262bcd92212efed4435bc5c6d75b10100632fb
João Sipaúba <joaosipauba@hotmail.com>
2020-06-13 08:36:49 -0300

    adicionando configuração do firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAnmejVGPQb02HpxHxC35nmZl-A2qEo-k0",
+  authDomain: "processo-seletivo-85337.firebaseapp.com",
+  databaseURL: "https://processo-seletivo-85337.firebaseio.com",
+  projectId: "processo-seletivo-85337",
+  storageBucket: "processo-seletivo-85337.appspot.com",
+  messagingSenderId: "409426416977",
+  appId: "1:409426416977:web:9fb7e48611c97cbc3d2f70",
+};
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);

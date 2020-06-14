b35368559f87ab50d5231fb07cfc069049025114
João Sipaúba <joaosipauba@hotmail.com>
2020-06-14 11:53:47 -0300

    Corrigindo erro que era gerado quando não havia curso cadastrado

--- src/pages/home/components/CursosList/cursosList.js
+++ src/pages/home/components/CursosList/cursosList.js
@@ -7,31 +7,35 @@ import { Segment, Table, Dimmer, Loader } from "semantic-ui-react";
 import { useHistory } from "react-router-dom";
 
 function CursosList() {
-  const history = useHistory()
+  const history = useHistory();
 
   const [cursos, setCursos] = useState([]);
   const [carregando, setCarregando] = useState(false);
-  
+
   useEffect(() => {
     setCarregando(true);
-    sessionStorage.setItem("curso", "" )
-    sessionStorage.setItem("idCurso", "" )
+    sessionStorage.setItem("curso", "");
+    sessionStorage.setItem("idCurso", "");
     var cursos = {};
     firebase
       .database()
       .ref("cursos")
       .on("value", (snapshot) => {
         cursos = snapshot.val();
-        setCursos(Object.values(cursos));
-        setCarregando(false);
+        if (cursos === null || cursos === undefined) {
+          setCursos(false);
+        } else {
+          setCursos(Object.values(cursos));
+          setCarregando(false);
+        }
       });
     return () => {};
   }, []);
 
   function rowClick(curso) {
-    sessionStorage.setItem("curso", curso.curso )
-    sessionStorage.setItem("idCurso", curso.codigo )
-    history.push("/Alunos")
+    sessionStorage.setItem("curso", curso.curso);
+    sessionStorage.setItem("idCurso", curso.codigo);
+    history.push("/Alunos");
   }
 
   return (
@@ -47,27 +51,37 @@ function CursosList() {
                 <Table celled selectable>
                   <Table.Header>
                     <Table.Row>
-                      <Table.HeaderCell><h3>Curso</h3></Table.HeaderCell>
+                      <Table.HeaderCell>
+                        <h3>Curso</h3>
+                      </Table.HeaderCell>
                       <Table.HeaderCell>Código</Table.HeaderCell>
                       <Table.HeaderCell>Data de cadastro</Table.HeaderCell>
                       <Table.HeaderCell>Carga horária(horas)</Table.HeaderCell>
                     </Table.Row>
                   </Table.Header>
-                  <Table.Body style={{cursor: 'pointer'}}>
-                    {cursos.map((curso, index) => (
-                      <Table.Row
-                        key={index}
-                        onClick={()=> rowClick(curso)}
-                      >
-                        <Table.Cell>
-                          <strong>{curso.curso}</strong>
-                        </Table.Cell>
-                        <Table.Cell>{curso.codigo}</Table.Cell>
-                        <Table.Cell>{curso.data}</Table.Cell>
-                        <Table.Cell>{curso.cargaHoraria}</Table.Cell>
-                      </Table.Row>
-                    ))}
-                  </Table.Body>
+                  {cursos === false ? (
+                    <h3 style={{ padding: "3%" }}>
+                      Não há cursos cadastrados no sistema
+                    </h3>
+                  ) : (
+                    <>
+                      <Table.Body style={{ cursor: "pointer" }}>
+                        {cursos.map((curso, index) => (
+                          <Table.Row
+                            key={index}
+                            onClick={() => rowClick(curso)}
+                          >
+                            <Table.Cell>
+                              <strong>{curso.curso}</strong>
+                            </Table.Cell>
+                            <Table.Cell>{curso.codigo}</Table.Cell>
+                            <Table.Cell>{curso.data}</Table.Cell>
+                            <Table.Cell>{curso.cargaHoraria}</Table.Cell>
+                          </Table.Row>
+                        ))}
+                      </Table.Body>
+                    </>
+                  )}
                   <CursosFooter />
                 </Table>
               </Segment.Group>

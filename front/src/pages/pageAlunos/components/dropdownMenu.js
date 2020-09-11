44e76047cb82bdcb468547bce2cc7013abeb27ce
João Sipaúba <joaosipauba@hotmail.com>
2020-09-11 11:27:25 -0300

    Função excluir curso.

--- front/src/pages/pageAlunos/components/dropdownMenu.js
+++ front/src/pages/pageAlunos/components/dropdownMenu.js
@@ -1,16 +1,24 @@
 import React from "react";
 
 // import firebase from "../../../services/firebase";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { excelDownload } from "../../../functions/excelDownload";
 
 import { Dropdown, Modal } from "semantic-ui-react";
 import ModalEditCurso from "./modalEditCurso";
+import Axios from "axios";
 
 function DropdownMenu() {
   const history = useHistory();
+  const {id} = useParams();
 
   function excluir() {
+    Axios.delete('http://localhost:3333/cursos/'+id).then(response=>{
+      console.log(response);
+      history.push('/')
+    }).catch(error=>{
+      console.log(error)
+    })
     // firebase
     //   .database()
     //   .ref(`/cursos/${curso}`)
